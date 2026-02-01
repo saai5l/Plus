ce8cc2d2262246385c2d0bea24a5e1d6bbc995ec
saai5l <ssyr0442@gmail.com>
2026-02-01 23:14:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -534,7 +534,7 @@ async function askAI() {
 
 
 const CLIENT_ID = '1453875994988380373'; 
-const REDIRECT_URI = 'http://127.0.0.1:5500/index.html'; 
+const REDIRECT_URI = 'https://saai5l.github.io/Plus/index.html';
 const ADMIN_IDS = ["1453875192009986166","1462236116785827851"]; 
 
 function login() {
